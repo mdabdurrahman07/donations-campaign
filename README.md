f16b1074730e71991e51f4cdf7e06b3f26bff1b1
mdabdurrahman07 <jamilrahman2001@gmail.com>
2023-09-26 01:30:35 +0600

    readme md file updated and donation page is functional and dynamic

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Donations Campaign
 
-Currently, two official plugins are available:
+This is a basic React-Router-based project that I have created for assignment number 8.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+## Project Features
+
+- Desktop Mobile Tablet Responsive 
+- Dynamics Donations Cards
+- Sweet Alert
+- Dynamic Routing
+
+
+
--- src/Pages/Donations/Donations.jsx
+++ src/Pages/Donations/Donations.jsx
@@ -4,21 +4,31 @@ import DonationsCards from "../DonationsCards/DonationsCards";
 
 const Donations = () => {
   const [doante , setdonate] = useState([])
+  const [nodata , setnodata] = useState(false)
   
   useEffect(()=>{
     const DonatedPrice = JSON.parse(localStorage.getItem('card'))
    
-      setdonate(DonatedPrice)
+      if(DonatedPrice){
+        setdonate(DonatedPrice)
+      }
+      else{
+        setnodata("You Have Nothing Donate Yet")
+      }
    
     
   },[])
     return (
-        <div className="max-w-screen-xl m-auto">
-              <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-6">
+        <div className="max-w-screen-xl m-auto mt-8">
+         {
+          nodata ? <p className="font-semibold text-2xl text-center mt-10">{nodata}</p> : <div>
+            <div className="grid md:grid-cols-1 lg:grid-cols-2 justify-center items-center gap-6">
                 {
                 doante.map(items => <DonationsCards key={items.id} items={items} ></DonationsCards>)
                 }
               </div>
+          </div>
+         }
         </div>
     );
 };
